6cb14f2b61100bbb881103a5cd9c126cbf793b0f
Emil <emfinne13@gmail.com>
2026-05-11 12:45:31 +0200

    additional changes

---
+++ src/bringup/launch/cell_bringup.launch.py
@@ -0,0 +1,44 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    control_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([FindPackageShare("control"), "launch", "start_robots.launch.py"])
+        ),
+        launch_arguments={
+            "Eevee_use_mock_hardware": "true",
+            "headless_mode":"true",
+            "launch_rviz": "false",
+        }.items(),
+    )
+
+    wait_robot_description = Node(
+        package="ur_robot_driver",
+        executable="wait_for_robot_description",
+        output="screen",
+    )
+
+    move_group_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([FindPackageShare("moveit_config"), "launch", "move_group.launch.py"])
+        )
+    )
+
+
+    return LaunchDescription([
+        control_launch,
+        wait_robot_description,
+        RegisterEventHandler(
+            OnProcessExit(
+                target_action=wait_robot_description,
+                on_exit=[move_group_launch],
+            )
+        ),
+    ])
